85ccd27ee69ee2d588a33045badb20a567808017
Samuel Besnier <sbesnier1901@gmail.com>
2017-06-15 11:48:55 +0200

    Traitement du formulaire de connexion

--- src/controllers/login-admin.php
+++ src/controllers/login-admin.php
@@ -1,12 +1,64 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Administrateur
+ * User: Samuel Besnier
  * Date: 15/06/2017
  * Time: 10:28
  */
 
+// Récupération des données postées
+$login = filter_input(
+    INPUT_POST,
+    'login',
+    FILTER_SANITIZE_STRING
+);
+
+$password = filter_input(
+    INPUT_POST,
+    'password',
+    FILTER_SANITIZE_STRING
+);
+
+$isSubmitted = filter_has_var(
+    INPUT_POST,
+    'submit'
+);
+
+$errors = [];
+
+// Validation des données si le formulaire est soumi
+if ($isSubmitted) {
+
+    if (empty($login)) {
+        $errors[] = "Vous devez saisir un identifiant";
+    }
+    if (empty($password)) {
+        $errors[] = "Vous devez saisir un mot de passe";
+    } else {
+
+    }
+
+    // Traitement des données s'il n'y a pas d'erreur
+    if (count($errors) == 0) {
+        $ok = $login == "admin" && $password == "123";
+
+        if ($ok) {
+            // Redirection
+            header(
+                "location:/?controller=home-admin"
+            );
+        } else {
+            $errors[] = "Vos informations d'identification sont incorrectes";
+        }
+    }
+}
+
 // Appel de la fontion renderView
 renderView(
-    'login-admin'
+    'login-admin',
+    [
+        'pageTitle' => 'Login administration',
+        'errors' =>$errors,
+        'login' => $login
+    ]
 );
--- src/views/login-admin.php
+++ src/views/login-admin.php
@@ -10,13 +10,24 @@
 <form method="post">
     <div class="form-group">
         <label>Votre identifiant</label>
-        <input type="text" name="login" class="form-control">
+        <input type="text" name="login" class="form-control" value="<?= $login ?>" required>
     </div>
     <div class="form-group">
         <label>Votre mot de passe</label>
-        <input type="password" name="password" class="form-control">
+        <input type="password" name="password" class="form-control" required>
     </div>
     <div class="form-group">
         <button type="submit" name="submit" class="btn btn-success">Valider</button>
     </div>
+
+    <?php if (count($errors) > 0) : ?>
+    <div class="alert alert-danger">
+        <ul>
+        <?php foreach ($errors as $error) : ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+        </ul>
+    </div>
+    <?php endif; ?>
+
 </form>
