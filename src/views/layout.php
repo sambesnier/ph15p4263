619cf16429ede081cc666b82cbcfc0e227406096
Samuel Besnier <sbesnier1901@gmail.com>
2017-06-15 09:50:03 +0200

    Application du framework Bootstrap au layout

--- src/views/layout.php
+++ src/views/layout.php
@@ -14,8 +14,22 @@
         <?= $pageTitle ?>
     </title>
 	<meta charset="utf-8">
+    <!-- Chargement du CSS de Bootstrap -->
+    <link rel="stylesheet" href="dependancies/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dependancies/bootstrap/dist/css/bootstrap-theme.min.css">
 </head>
-<body>
-    <?= $content ?>
+<body class="container-fluid">
+
+    <!-- Contenu de l'application -->
+    <section class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <?= $content ?>
+        </div>
+    </section>
+
+    <!-- Chargement du Javascript de Bootstrap -->
+    <script src="dependancies/jquery/dist/jquery.min.js"></script>
+    <script src="dependancies/bootstrap/dist/js/bootstrap.min.js"></script>
+
 </body>
 </html>
