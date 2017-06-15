36bbc299191140dc8feb0ac85ff09fbb50d2ab81
Samuel Besnier <sbesnier1901@gmail.com>
2017-06-15 16:22:17 +0200

    Ajout et suppression de compétences

--- src/controllers/home-admin.php
+++ src/controllers/home-admin.php
@@ -6,10 +6,73 @@
  * Time: 11:49
  */
 
+// Récupération de la liste des compétences
+// Chemin vers le fichier json
+$filePath = ROOT_PATH. '/src/data/competences.json';
+
+// Récupération des données postées
+$skill = filter_input(
+    INPUT_POST,
+    'skill',
+    FILTER_SANITIZE_STRING
+);
+
+$isSubmitted = filter_has_var(
+    INPUT_POST,
+    'submit'
+);
+
+$index = 0;
+$isClicked = false;
+
+$errors = [];
+
+// Récupération des données sous la forme d'un tableau
+$data = json_decode(file_get_contents($filePath), true);
+
+for ($i = 0; $i < count($data['skills']); $i++) {
+    if (filter_has_var(
+        INPUT_POST,
+        $data['skills'][$i]
+    )) {
+        $index = $i;
+        $isClicked = true;
+    }
+}
+
+if ($isSubmitted && !$isClicked) {
+
+    if (empty($skill)) {
+        $errors[] = "Vous devez saisir une compétence";
+    } else if (!in_array($skill, $data['skills'])) {
+        // Traitement des données s'il n'y a pas d'erreur
+        // Récupération des données sous la forme d'un tableau
+        array_push($data['skills'], $skill);
+        file_put_contents($filePath, json_encode($data));
+        // Redirection pour éviter de reposter les données
+        header("location:/?controller=home-admin");
+    } else {
+        $errors[] = "Cette compétence existe déjà";
+    }
+}
+
+if ($isClicked) {
+    // Récupération des données sous la forme d'un tableau
+    array_splice($data['skills'], $index, 1);
+    $data = json_encode($data);
+    file_put_contents($filePath, $data);
+    header("location:/?controller=home-admin");
+}
+
+// Récupération des données sous la forme d'un tableau
+$data = json_decode(file_get_contents($filePath), true);
+
 // Appel de la fontion renderView
 renderView(
     'home-admin',
     [
-        'pageTitle' => 'Interface d\'administration'
+        'pageTitle' => 'Interface d\'administration',
+        'skills' => $data['skills'],
+        'errors' => $errors
     ]
 );
--- src/views/home-admin.php
+++ src/views/home-admin.php
@@ -7,4 +7,42 @@
  */
 ?>
 
-<h1>Bienvenue sur la page d'administration</h1>
+<h1>Espace d'administration</h1>
+
+<h2>Liste des compétences</h2>
+<div class="col-md-8">
+    <table class="table table-bordered table-striped">
+        <tr>
+            <th>Compétence</th>
+            <th></th>
+        </tr>
+        <form method="post">
+        <?php foreach ($skills as $skill) : ?>
+        <tr>
+            <td><?= $skill ?></td>
+            <td><button name="<?= $skill ?>" type="submit" class="btn btn-danger center-block glyphicon glyphicon-trash"></button></td>
+        </tr>
+        <?php endforeach; ?>
+        </form>
+    </table>
+</div>
+<div class="col-md-4 well">
+    <form method="post">
+        <div class="form-group">
+            <label>Nouvelle compétence</label>
+            <input type="text" name="skill" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <button type="submit" name="submit" class="btn btn-success">Ajouter</button>
+        </div>
+    </form>
+    <?php if (count($errors) > 0) : ?>
+        <div class="alert alert-danger">
+            <ul>
+                <?php foreach ($errors as $error) : ?>
+                    <li><?= $error ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+</div>
