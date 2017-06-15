ad4ad3f38173be0c8711470dae5259e3bb0f93d5
Samuel Besnier <sbesnier1901@gmail.com>
2017-06-15 11:54:11 +0200

    Mise en place de la page d'administration

---
+++ src/controllers/home-admin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samuel Besnier
+ * Date: 15/06/2017
+ * Time: 11:49
+ */
+
+// Appel de la fontion renderView
+renderView(
+    'home-admin',
+    [
+        'pageTitle' => 'Bienvenue sur l\'interface d\'administration'
+    ]
+);
---
+++ src/views/home-admin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samuel Besnier
+ * Date: 15/06/2017
+ * Time: 11:50
+ */
+?>
+
+<h1>Bienvenue sur la page d'administration</h1>
