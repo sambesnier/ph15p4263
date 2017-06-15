0c0608afc74084831a3f00ea2b4dfa88ae05e66e
Samuel Besnier <sbesnier1901@gmail.com>
2017-06-15 19:34:16 +0200

    Quiz fonctionnel

--- src/controllers/quiz.php
+++ src/controllers/quiz.php
@@ -14,15 +14,59 @@ $filePath = ROOT_PATH. '/src/data/quiz.json';
 // Récupération des données sous la forme d'un tableau
 $data = json_decode(file_get_contents($filePath), true);
 
-$size = count($data['quiz']);
+$errors = [];
 
+$correction = [];
 
+$isSubmitted = filter_has_var(
+    INPUT_POST,
+    'submit'
+);
+
+if ($isSubmitted) {
+	$size = count($data['quiz']);	
+	$score = 0;
+	for ($i=0; $i < $size; $i++) { 
+		$indexRightAnswer = $data['quiz'][$i]['bonneReponse'];
+		$rightAnswer = $data['quiz'][$i]['reponses'][0][$indexRightAnswer];
+
+		if($indexRightAnswer != $_POST['question'.($i+1)]) {
+			$correction[] = 
+			[ 
+				"reponse" => 
+				[
+					"good" => "no", 
+					"rightAnswer" => $rightAnswer, 
+					"question" => $i+1
+				] 
+			];
+		} else {
+			$correction[] = 
+			[ 
+				"reponse" => 
+				[
+					"good" => "yes", 
+					"rightAnswer" => $rightAnswer, 
+					"question" => $i+1
+				] 
+			];
+			$score++;
+		}
+	}
+	$correction[] = 
+		[
+			"score" => $score,
+			"total" => $size
+		];
+}
 
 // Appel de la fontion renderView
 renderView(
     'quiz',
     [
         'pageTitle' => 'Quiz',
-        'quiz' => $data['quiz']
+        'quiz' => $data['quiz'],
+        'errors' => $errors,
+        'correction' => $correction ?? []
     ]
 );
--- src/views/quiz.php
+++ src/views/quiz.php
@@ -11,12 +11,26 @@
 
 <h2>Questions</h2>
 
+<?php if (!empty($correction)) : ?>
+    <div class="alert alert-success">
+        Félicitations, vous avez obtenu un score de <?= end($correction)['score'] ?>/<?= end($correction)['total'] ?> !
+    </div>
+<?php endif; ?>
+
 <form method="post">
     <?php foreach ($quiz as $num => $ask) : ?>
         <h3><?= $ask['question'] ?></h3>
+        <?php if (!empty($correction)) : ?>
+            Bonne réponse : 
+            <p style="<?= $correction[$num]['reponse']['good']=="yes"?
+            'color: green':
+            'color:red'; ?>">
+                <?= $correction[$num]['reponse']['rightAnswer'] ?>
+            </p>
+        <?php endif; ?>
         <div class="form-group">
         <?php foreach ($ask['reponses'][0] as $key => $rep) : ?>
-            <input type="radio" name="question<?= $num ?>[]" value="<?= $key ?>"><?= $rep ?><br>
+            <input type="radio" name="question<?= $num+1 ?>" value="<?= $key ?>" required> <?= $rep ?><br>
         <?php endforeach; ?>
         </div>
     <?php endforeach; ?>
@@ -24,3 +38,12 @@
         <button type="submit" name="submit" class="btn btn-success">Ajouter</button>
     </div>
 </form>
+<?php if (count($errors) > 0) : ?>
+    <div class="alert alert-danger">
+        <ul>
+        <?php foreach ($errors as $error) : ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
